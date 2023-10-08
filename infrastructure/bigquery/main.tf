a64c5b7d8e1f2372b2c113f7145dd231d2625612
Alexander Potts <ajpotts01@gmail.com>
2023-10-08 22:54:41 +1100

    Messing around with datasets/external tables

--- infrastructure/bigquery/main.tf
+++ infrastructure/bigquery/main.tf
@@ -1,11 +1,34 @@
+data "local_file" "airports_schema" {
+  filename = "${path.module}/airports_schema.json"
+}
+
 resource "google_bigquery_dataset" "dsongcp_dataset" {
-    project = var.project_id
-    dataset_id = "dsongcp"
+  project    = var.project_id
+  dataset_id = "dsongcp"
+  location   = var.region
+}
+
+resource "google_bigquery_table" "dsongcp_airports" {
+  dataset_id = google_bigquery_dataset.dsongcp_dataset.dataset_id
+  table_id   = "airports"
+  project    = var.project_id
+
+  external_data_configuration {
+    source_uris   = ["${var.ingestion_storage_bucket_uri}/airports/raw/airports.csv"]
+    autodetect    = true
+    source_format = "CSV"
+    csv_options {
+      skip_leading_rows = 1
+      quote             = "\""
+    }
+  }
+
+  schema = data.local_file.airports_schema.content
 }
 
 resource "google_bigquery_dataset_iam_member" "ingestion_service_account_bq_dataset" {
   project    = var.project_id
-  dataset_id = "dsongcp"
+  dataset_id = google_bigquery_dataset.dsongcp_dataset.dataset_id
   role       = "roles/bigquery.dataOwner"
   member     = "serviceAccount:${var.ingestion_service_account_email}"
 }
--- infrastructure/service-apis/main.tf
+++ infrastructure/service-apis/main.tf
@@ -49,3 +49,8 @@ resource "google_project_service" "cloud_functions_service" {
   project = var.project_id
   service = "cloudfunctions.googleapis.com"
 }
+
+resource "google_project_service" "bigquery_datatransfer_service" {
+  project = var.project_id
+  service = "bigquerydatatransfer.googleapis.com"
+}
