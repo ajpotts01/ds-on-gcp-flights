70a6a24cd521e8d33505a059ae50f90056ba545c
Alexander Potts <ajpotts01@gmail.com>
2023-10-12 22:10:41 +1100

    Beam pipeline - direct runner works

--- data_extract/ingest_airports/src/ingest_airports.py
+++ data_extract/ingest_airports/src/ingest_airports.py
@@ -8,8 +8,17 @@ def main():
         airports: PValue = (
             pipeline
             | beam.io.ReadFromText("airports.csv.gz")
-            | beam.Map(lambda line: next(csv.reader(line)))
-            | beam.Map(lambda fields: (fields[0], fields[21], fields[26]))
+            | beam.Map(lambda line: next(csv.reader([line])))
+            # Airport code, then tuple/pair of lat/lon
+            | beam.Map(lambda fields: (fields[0], (fields[21], fields[26])))
+        )
+
+        (
+            airports
+            | beam.Map(
+                lambda airport_data: f"{airport_data[0]},{','.join(airport_data[1])}"
+            )
+            | beam.io.WriteToText("extracted_airports")
         )
 
 
